ccc0232ecae34f1d53b2f3eb6da93ae671ec17c5
Clément <clab60917@eleve.isep.fr>
2023-04-14 19:37:27 +0200

    continuité du dev

---
+++ components/about.vue
@@ -0,0 +1,56 @@
+<template>
+    <section id="about" class="hero">
+      <div class="about-container">
+        <div class="about-image">
+          <img src="images/pc.jpg" alt="Photo de profil de Clément John" class="profile-image" />
+        </div>
+        <div class="about-content">
+          <h1>Clément Arthaud-Berthet</h1>
+          <h2>À propos de moi</h2>
+          <div class="about-text">
+            <p>Étudiant en école d'ingénieur, je suis passionné par la cybersécurité. Je participe fréquemment à des CTF et j'ai pu réaliser différents pentests auprès d'entreprises.</p>
+          </div>
+          <div class="experience">
+            <h3>Expérience de leadership</h3>
+            <div class="experience-item">
+              <p>Actuellement président du plus grand Innovation Hub étudiant de France : Garage Isep, avec plus de 80 personnes à manager.</p>
+            </div>
+          </div>
+          <div class="interests">
+            <h3>Intérêts supplémentaires</h3>
+            <div class="interests-container">
+              <div class="interest-item">
+                <i class="fas fa-cubes"></i>
+                <p>Blockchain</p>
+              </div>
+              <div class="interest-item">
+                <i class="fas fa-brain"></i>
+                <p>Intelligence artificielle</p>
+              </div>
+            </div>
+            <p>Les domaines de la blockchain et de l'intelligence artificielle m'intéressent également, je me forme sur ces sujets en autodidacte.</p>
+          </div>
+          <div class="about-contact">
+            <h3>Contactez-moi</h3>
+            <a href="mailto:clement@example.com" class="email-link">clement@example.com</a>
+            <div class="social-icons">
+              <a href="https://www.linkedin.com/in/clement" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a>
+              <a href="https://github.com/clement" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+              <a href="https://twitter.com/clement" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    name: 'About'
+  }
+  </script>
+  
+  <style scoped>
+  /* Insérez ici vos styles CSS spécifiques au composant About */
+  </style>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,36 @@
 <template>
-  <Tutorial/>
+  <div>
+    <Header />
+    <About />
+    <Timeline />
+    <Projects />
+    <Contact />
+    <Footer />
+  </div>
 </template>
 
 <script>
+import Header from '~/components/Header.vue';
+import About from '~/components/About.vue';
+import Timeline from '~/components/Timeline.vue';
+import Projects from '~/components/Projects.vue';
+import Contact from '~/components/Contact.vue';
+import Footer from '~/components/Footer.vue';
+import XSS from '../components/XSS.vue';
+
 export default {
-  name: 'IndexPage'
+  components: {
+    Header,
+    About,
+    Timeline,
+    Projects,
+    Contact,
+    Footer
+    XSS
+  }
 }
 </script>
+
+<style>
+/* Insérez ici vos styles CSS globaux */
+</style>
