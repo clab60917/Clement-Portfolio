563f29cc76c62961fe8fc9caab3c389b3cacbd6d
Clément <clab60917@eleve.isep.fr>
2023-04-15 00:05:39 +0200

    timeline working v1

--- components/timeline.vue
+++ components/timeline.vue
@@ -1,34 +1,27 @@
 <template>
-    <section id="timeline">
-      <h2>Parcours</h2>
-      <div class="timeline">
-        <div class="container left">
-          <div class="content">
-            <h3>Diplôme d'ingénieur</h3>
-            <img class="logo" src="/images/1280px-Institut_supérieur_d'électronique_de_Paris.svg.png" alt="Logo Expérience 1" />
-            <p>2024 : diplôme d'ingénieur du numérique spécialisé en Cybersécurité.</p>
-          </div>
-        </div>
-        <div class="container right">
-          <div class="content">
-            <h3>Stage Pentest Groupe La Poste</h3>
-            <img class="logo" src="/images/Logo-groupe-la-poste-2021.png" alt="Logo Expérience 2" />
-            <p>Stage de 6 mois en tant que pentesteur web + réseau au sein de la BGPN du groupe La Poste.</p>
-          </div>
-        </div>
-        <div class="container left">
-          <div class="content">
-            <h3>Président Innovation Hub</h3>
-            <img class="logo" src="/images/Garage ISEP Logo Horizontal Black (1).png" alt="Logo Expérience 3" />
-            <p>Président de Garage Isep pour le mandat 2023-2024 du plus grand Innovation Hub étudiant de France.</p>
-          </div>
-        </div>
-        <div class="container right">
-          <div class="content">
-            <h3>Erasmus CVUT Prague</h3>
-            <img class="logo" src="/images/Czech_Technical_University_in_Prague_logo_with_text.gif" alt="Logo Expérience 4" />
-            <p>Échange universitaire de 6 mois à l'école Polytechnique de Prague.</p>
-          </div>
+    <section id="timeline" class="bg-gray-100 py-12">
+      <div class="container mx-auto px-4">
+        <h2 class="text-3xl font-semibold mb-8 text-center">Parcours</h2>
+        <div class="relative m-8">
+          <div class="border-r-2 border-black absolute h-full top-0" style="left: 15px"></div>
+          <ul class="list-none m-0 p-0">
+            <li v-for="(item, index) in items" :key="index" class="mb-12">
+              <div class="flex items-center mb-1">
+                <div
+                  class="bg-black rounded-full h-8 w-8 border-4 border-white z-10"
+                  :class="{ 'ml-0 -mr-4': index % 2 === 0, 'ml-auto mr-0 -ml-4': index % 2 === 1 }"
+                ></div>
+                <div
+                  class="w-full bg-white p-6 rounded-lg shadow-lg border border-gray-200"
+                  :class="{ 'ml-4': index % 2 === 0, 'mr-4': index % 2 === 1 }"
+                >
+                  <h3 class="text-2xl font-semibold mb-2">{{ item.title }}</h3>
+                  <img class="logo h-16 w-auto mb-4" :src="item.logo" :alt="`Logo ${item.title}`" />
+                  <p class="leading-6">{{ item.description }}</p>
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </section>
@@ -37,10 +30,34 @@
   <script>
   export default {
     name: "Timeline",
+    data() {
+      return {
+        items: [
+          {
+            title: "Diplôme d'ingénieur",
+            logo: "/images/1280px-Institut_supérieur_d'électronique_de_Paris.svg.png",
+            description: "2024 : diplôme d'ingénieur du numérique spécialisé en Cybersécurité.",
+          },
+          {
+            title: "Stage Pentest Groupe La Poste",
+            logo: "/images/Logo-groupe-la-poste-2021.png",
+            description: "Stage de 6 mois en tant que pentesteur web + réseau au sein de la BGPN du groupe La Poste.",
+          },
+          {
+            title: "Président Innovation Hub",
+            logo: "/images/Garage ISEP Logo Horizontal Black (1).png",
+            description: "Président de Garage Isep pour le mandat 2023-2024 du plus grand Innovation Hub étudiant de France.",
+          },
+          {
+            title: "Erasmus CVUT Prague",
+            logo: "/images/Czech_Technical_University_in_Prague_logo_with_text.gif",
+            description: "Échange universitaire de 6 mois à l'école Polytechnique de Prague.",
+          },
+        ],
+      };
+    },
   };
   </script>
   
-  <style scoped>
-  /* Ajoutez ici vos styles CSS spécifiques à ce composant */
-  </style>
+  <style scoped></style>
   
