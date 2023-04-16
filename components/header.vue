7f7a8aa6ab24517cef74c424b6c68fffc7f48d48
Clément <clab60917@eleve.isep.fr>
2023-04-16 20:53:29 +0200

    working

--- components/header.vue
+++ components/header.vue
@@ -1,32 +1,32 @@
 <template>
-    <header class="bg-white shadow-md">
-      <div class="container mx-auto px-4">
-        <div class="flex justify-between items-center py-6">
-          <div class="text-3xl font-semibold">
-            Portfolio de Clément
-          </div>
-          <nav class="hidden md:flex space-x-8">
-            <a href="#about" class="text-lg hover:text-gray-700 transition-colors">À propos</a>
-            <a href="#skills" class="text-lg hover:text-gray-700 transition-colors">Compétences</a>
-            <a href="#timeline" class="text-lg hover:text-gray-700 transition-colors">Parcours</a>
-            <a href="#projects" class="text-lg hover:text-gray-700 transition-colors">Projets</a>
-            <a href="#contact" class="text-lg hover:text-gray-700 transition-colors">Contact</a>
-          </nav>
-          <button @click="toggleMenu" class="md:hidden focus:outline-none">
-            <i class="fas fa-bars text-xl"></i>
-          </button>
+  <header class="bg-white shadow-md">
+    <div class="container mx-auto px-4">
+      <div class="flex justify-between items-center py-6">
+        <div class="text-3xl font-semibold">
+          Portfolio de Clément
         </div>
-        <div v-if="menuOpen" class="flex flex-col space-y-4 mt-4 md:hidden">
-          <a href="#about" @click="closeMenu" class="text-lg hover:text-gray-700 transition-colors">À propos</a>
-          <a href="#skills" @click="closeMenu" class="text-lg hover:text-gray-700 transition-colors">Compétences</a>
-          <a href="#timeline" @click="closeMenu" class="text-lg hover:text-gray-700 transition-colors">Parcours</a>
-          <a href="#projects" @click="closeMenu" class="text-lg hover:text-gray-700 transition-colors">Projets</a>
-          <a href="#contact" @click="closeMenu" class="text-lg hover:text-gray-700 transition-colors">Contact</a>
-        </div>
+        <nav class="hidden md:flex space-x-8">
+          <a href="#about" class="text-lg hover:text-gray-700 transition-colors">À propos</a>
+          <a href="#skills" class="text-lg hover:text-gray-700 transition-colors">Compétences</a>
+          <a href="#timeline" class="text-lg hover:text-gray-700 transition-colors">Parcours</a>
+          <a href="#projects" class="text-lg hover:text-gray-700 transition-colors">Projets</a>
+          <a href="#contact" class="text-lg hover:text-gray-700 transition-colors">Contact</a>
+        </nav>
+        <button @click="toggleMenu" class="md:hidden focus:outline-none">
+          <i class="fas fa-bars text-xl"></i>
+        </button>
       </div>
-    </header>
-  </template>
-  
+      <div v-if="menuOpen" class="flex flex-col space-y-4 mt-4 md:hidden">
+        <a href="#about" @click="closeMenu" class="text-lg hover:text-gray-700 transition-colors">À propos</a>
+        <a href="#skills" @click="closeMenu" class="text-lg hover:text-gray-700 transition-colors">Compétences</a>
+        <a href="#timeline" @click="closeMenu" class="text-lg hover:text-gray-700 transition-colors">Parcours</a>
+        <a href="#projects" @click="closeMenu" class="text-lg hover:text-gray-700 transition-colors">Projets</a>
+        <a href="#contact" @click="closeMenu" class="text-lg hover:text-gray-700 transition-colors">Contact</a>
+      </div>
+    </div>
+  </header>
+</template>
+
   
   <script>
   export default {
