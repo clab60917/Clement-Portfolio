5acb9008da1cb0c31e3a6e2ce899170b4e7eef45
Clément <clab60917@eleve.isep.fr>
2023-04-17 11:36:40 +0200

    widget add, not fully working

--- components/about.vue
+++ components/about.vue
@@ -1,7 +1,8 @@
 <template>
     <section id="about" class="hero bg-gray-100 py-16">
       <div class="about-container mx-auto max-w-screen-2xl rounded-lg bg-white shadow-lg p-8 animate-fade-in">
-        <div class="flex flex-wrap justify-between items-start">
+        <div class="flex flex-wrap justify-between items-start md:items-stretch lg:items-start">
+
           <div class="about-image w-full md:w-1/4 md:pr-8 mb-4">
             <img src="images/pc.jpg" alt="Photo de profil de Clément John" class="profile-image w-full h-auto rounded-lg shadow-lg transition-all duration-500 ease-in-out transform hover:scale-105" />
           </div>
@@ -17,7 +18,7 @@
             </div>
           </div>
         </div>
-        <div class="about-content w-full md:w-1/2 mt-10 md:mt-0">
+        <div class="about-content w-full md:w-2/5 mt-10 md:mt-0">
           <h1 class="text-4xl font-bold mb-4">Clément Arthaud-Berthet</h1>
           <h2 class="text-2xl font-semibold mb-6">À propos de moi</h2>
           <div class="about-text mb-8">
@@ -25,32 +26,78 @@
             Doté d'une grande capacité d'organisation et d'adaptation, je sais m'intégrer dans de nouveaux contextes de travail grace à un véritable esprit d'équipe.</p>
           </div>
           <div class="experience mb-8">
-            <h3 class="text-xl font-semibold mb-4">Recherche de Stage</h3>
-            <div class="experience-item">
-              <p class="text-base leading-6">Je cherche actuellement un stage de fin d'études (M2) pour une durée de 6 mois, dans le domaine de la cybersécurité. Je suis attiré par les postes suivants : analyste cyber, pentesteur, chef de projet cyber. Je reste ouvert à toute proposition tierce en rapport avec mon domaine d'activité.</p>
-            </div>
-        </div>
-        <div class="interests mb-8">
-          <h3 class="text-xl font-semibold mb-4">Intérêts supplémentaires</h3>
-          <div class="interests-container flex items-center mb-4">
-            <div class="interest-item mr-4">
-              <i class="fas fa-cubes text-2xl"></i>
-              <p class="mt-1 text-base font-medium">Blockchain</p>
-            </div>
-            <div class="interest-item">
-              <i class="fas fa-brain text-2xl"></i>
-              <p class="mt-1 text-base font-medium">Intelligence artificielle</p>
-            </div>
+          <h3 class="text-xl font-semibold mb-4">Recherche de Stage</h3>
+          <div class="experience-item">
+            <p class="text-base leading-6">Je cherche actuellement un stage de fin d'études (M2) pour une durée de 6 mois, dans le domaine de la cybersécurité. Je suis attiré par les postes suivants : analyste cyber, pentesteur, chef de projet cyber. Je reste ouvert à toute proposition tierce en rapport avec mon domaine d'activité.</p>
           </div>
-          <p class="text-base leading-6"></p>
+      </div>
+      <div class="interests mb-8">
+        <h3 class="text-xl font-semibold mb-4">Intérêts supplémentaires</h3>
+        <div class="interests-container flex items-center mb-4">
+          <div class="interest-item mr-4">
+            <i class="fas fa-cubes text-2xl"></i>
+            <p class="mt-1 text-base font-medium">Blockchain</p>
+          </div>
+          <div class="interest-item">
+            <i class="fas fa-brain text-2xl"></i>
+            <p class="mt-1 text-base font-medium">Intelligence artificielle</p>
+          </div>
         </div>
-        <a href="path/to/your/cv.pdf" download class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded transition-colors">Télécharger le CV en PDF</a>
-    </div>
+        <p class="text-base leading-6"></p>
+      </div>
+      <a href="path/to/your/cv.pdf" download class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded transition-colors">Télécharger le CV en PDF</a>
+  </div>
+  <div class="widget-container w-11/12 h-full md:w-1/3 md:pl-8 mt-10 md:mt-0">
+  <div class="widget bg-blue-500 p-4 rounded-lg shadow-md h-[900px] overflow-y-auto">
+    <h3 class="text-xl font-semibold text-white mb-4">Veille en cybersécurité</h3>
+    <div class="widget-content space-y-4">
+      <!-- News Item 1 -->
+      <div class="news-item bg-white p-4 rounded-lg shadow-md">
+        <div class="flex flex-col md:flex-row">
+          <img src="https://www.scmagazine.com/_next/image?url=https%3A%2F%2Ffiles.scmagazine.com%2Fwp-content%2Fuploads%2F2022%2F03%2FPicture1-3.jpg&w=750&q=75" alt="Ransomware et Cybersécurité" class="w-full h-32 object-cover rounded mb-2 md:mb-0 md:mr-4">
+          <div class="flex flex-col justify-between">
+            <h4 class="text-lg font-semibold">Ransomware et Cybersécurité</h4>
+            <p class="text-base leading-6">Comprendre le ransomware et les meilleures pratiques pour se protéger contre les cyberattaques.</p>
+            <a href="https://www.scmagazine.com/sw-article/asset-management/security-hygiene-doing-all-the-little-things-right-all-the-time" class="text-blue-500 hover:text-blue-700">En savoir plus</a>
+          </div>
         </div>
       </div>
-    
+      <!-- News Item 2 -->
+      <div class="news-item bg-white p-4 rounded-lg shadow-md">
+        <div class="flex flex-col md:flex-row">
+          <img src="https://www.scmagazine.com/_next/image?url=https%3A%2F%2Ffiles.scmagazine.com%2Fwp-content%2Fuploads%2F2022%2F10%2FPicture1.jpg&w=750&q=75" alt="Formation en cybersécurité" class="w-full h-32 object-cover rounded mb-2 md:mb-0 md:mr-4">
+          <div class="flex flex-col justify-between">
+            <h4 class="text-lg font-semibold">Formation en cybersécurité</h4>
+            <p class="text-base leading-6">Découvrez les meilleures formations en cybersécurité pour développer vos compétences et progresser dans votre carrière.</p>
+            <a href="https://www.scmagazine.com/sw-article/cloud-security/the-unique-challenges-of-companies-born-in-the-cloud" class="text-blue-500 hover:text-blue-700">En savoir plus</a>
+          </div>
+        </div>
+      </div>
+      <!-- News Item 3 -->
+      <div class="news-item bg-white p-4 rounded-lg shadow-md">
+        <div class="flex flex-col md:flex-row">
+            <img src="https://www.scmagazine.com/_next/image?url=https%3A%2F%2Ffiles.scmagazine.com%2Fwp-content%2Fuploads%2F2022%2F04%2FPicture1-2.jpg&w=750&q=75" alt="Les métiers de la cybersécurité" class="w-full h-32 object-cover rounded mb-2 md:mb-0 md:mr-4">
+          <div class="flex flex-col justify-between">
+            <h4 class="text-lg font-semibold">Les métiers de la cybersécurité</h4>
+            <p class="text-base leading-6">Découvrez les différents métiers de la cybersécurité et les compétences requises pour y accéder.</p>
+            <a href="https://www.scmagazine.com/sw-article/zero-trust/forensic-challenges-for-security-professionals" class="text-blue-500 hover:text-blue-700">En savoir plus</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+      </div>
+    </div>
   </section>
 </template>
+
 <script>
 export default {
   name: 'About'
@@ -123,4 +170,15 @@ export default {
 .animate-fade-in {
   animation: fade-in 1s forwards;
 }
+
+
+@media (min-width: 1024px) {
+  .widget-container {
+    align-self: flex-start;
+  }
+}
+
 </style>
+
+
+  
