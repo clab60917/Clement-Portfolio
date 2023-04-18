eb30313d084633a2505332e750e83c9f73470016
Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>
2023-04-18 13:45:40 +0200

    test

---
+++ .output/server/chunks/app/_nuxt/about-styles.7de47449.mjs
@@ -0,0 +1,6 @@
+const about_vue_vue_type_style_index_0_scoped_8c646aa7_lang = ".icon-github[data-v-8c646aa7],.icon-linkedin[data-v-8c646aa7],.icon-twitter[data-v-8c646aa7]{align-items:center;background-color:#f3f4f6;border-radius:50%;display:inline-flex;height:50px;justify-content:center;width:50px}.icon-github[data-v-8c646aa7]:hover,.icon-linkedin[data-v-8c646aa7]:hover,.icon-twitter[data-v-8c646aa7]:hover{background-color:#e5e7eb}.email-link[data-v-8c646aa7],.social-icons a[data-v-8c646aa7]{transition:all .3s}@keyframes slide-in-from-left-8c646aa7{0%{opacity:0;transform:translateX(-100%)}to{opacity:1;transform:translateX(0)}}.animate-slide-in-from-left[data-v-8c646aa7]{animation:slide-in-from-left-8c646aa7 1s forwards}@keyframes slide-in-from-right-8c646aa7{0%{opacity:0;transform:translateX(100%)}to{opacity:1;transform:translateX(0)}}.animate-slide-in-from-right[data-v-8c646aa7]{animation:slide-in-from-right-8c646aa7 1s forwards}@keyframes fade-in-8c646aa7{0%{opacity:0}to{opacity:1}}.animate-fade-in[data-v-8c646aa7]{animation:fade-in-8c646aa7 1s forwards}@media (min-width:1024px){.widget-container[data-v-8c646aa7]{align-self:flex-start}}";
+
+const aboutStyles_7de47449 = [about_vue_vue_type_style_index_0_scoped_8c646aa7_lang];
+
+export { aboutStyles_7de47449 as default };
+//# sourceMappingURL=about-styles.7de47449.mjs.map
---
+++ .output/server/chunks/app/_nuxt/skills-styles.ddfdc864.mjs
@@ -0,0 +1,6 @@
+const skills_vue_vue_type_style_index_0_scoped_13ff06b6_lang = "";
+
+const skillsStyles_ddfdc864 = [skills_vue_vue_type_style_index_0_scoped_13ff06b6_lang];
+
+export { skillsStyles_ddfdc864 as default };
+//# sourceMappingURL=skills-styles.ddfdc864.mjs.map
